81c034992edcdd17dedf6aa7a42967653a2a2080
edgaregidio <edgarsiqueirasi@gmail.com>
2020-05-06 00:17:19 -0300

    rotas index, create, read

--- src/controller/courseControlle.js
+++ src/controller/courseControlle.js
@@ -1,11 +1,80 @@
-var course = [{
-  id: 2,
+var dbCourses = [
+  {
+    id: 1,
     name: 'Sistema de Informação',
     nameTeacher: 'Gustavo',
     nbrStudent: 5,
     duration: '4 anos'
-},
+  },
+  {
+    id: 2,
+    name: 'Engenharia de Software',
+    nameTeacher: 'Paulo',
+    nbrStudent: 10,
+    duration: '5 anos'
+  },
+  {
+    id: 3,
+    name: 'Engenharia de Computação',
+    nameTeacher: 'Luiz Mário',
+    nbrStudent: 15,
+    duration: '5 anos'
+  },
+  {
+    id: 4,
+    name: 'Análise de Sistema de Informação',
+    nameTeacher: 'Dianne',
+    nbrStudent: 20,
+    duration: '2 anos'
+  },
+  {
+    id: 5,
+    name: 'Ciência da Computação',
+    nameTeacher: 'Jair',
+    nbrStudent: 25,
+    duration: '5 anos'
+  },
+];
 
+module.exports = {
+  index: async (req, res) => {
+    return res.json(dbCourses);
+  },
 
+  create: async (req, res) => {
+    const { name, nameTeacher, nbrStudent, duration } = req.body;
 
-]
+    if (!name || !nameTeacher || !nbrStudent || !duration) {
+      return res.status(400).json({ error: 'Fill in the fields correctly.' })
+    }
+
+    const lastId = dbCourses.reduce((id, course) => {
+      return course.id > id ? course.id : id
+    }, 0);
+
+    const newCourse = {
+      id: lastId + 1,
+      name,
+      nameTeacher,
+      nbrStudent,
+      duration
+    }
+
+    dbCourses.push(newCourse)
+
+    return res.json(newCourse)
+    
+  },
+
+  read: async (req, res) => {
+
+  },
+
+  update: async (req, res) => {
+
+  },
+
+  delete: async (req, res) => {
+
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,10 @@
 const express = require('express');
+const courseController = require('./controller/courseControlle');
  
 const routes = express.Router();
 
-routes.get('/cursos', (req, res) => {
-  return res.json({
-    
-  })
-})
+routes.get('/cursos', courseController.index);
+routes.post('/cursos', courseController.create);
+routes.get('/cursos/:id', courseController.read);
 
 module.exports = routes;
