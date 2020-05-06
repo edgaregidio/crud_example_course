fb2180b448cf6444d0eda7b3eda753bdbaec4c2e
edgaregidio <edgarsiqueirasi@gmail.com>
2020-05-05 23:53:21 -0300

    crud course

---
+++ src/controller/courseControlle.js
@@ -0,0 +1,11 @@
+var course = [{
+  id: 2,
+    name: 'Sistema de Informação',
+    nameTeacher: 'Gustavo',
+    nbrStudent: 5,
+    duration: '4 anos'
+},
+
+
+
+]
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+ 
+const routes = express.Router();
+
+routes.get('/cursos', (req, res) => {
+  return res.json({
+    
+  })
+})
+
+module.exports = routes;
